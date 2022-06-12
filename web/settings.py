c70a6dd0f78b59758890e848b7f0c693548ca217
MrPowerUp82 <gustavogostozo8@gmail.com>
2022-06-12 18:20:01 -0300

    update

--- web/settings.py
+++ web/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'sl8jrm!iywpp^_0e%!rweflwna!_3r*w31n!iu_5h6@)28pe98'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '.herokuapp.com','127.0.0.1']
 
