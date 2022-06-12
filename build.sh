8695cd8608950cd3664af5e9b9744548af948c57
MrPowerUp82 <gustavogostozo8@gmail.com>
2022-06-12 14:40:54 -0300

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 # build.sh
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', '12345678')" | python3.9 manage.py shell
--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import login, logout, msg, test, search, invites, profile, signup, profile_user, post, ScoreView, score, game
+from .views import login, logout, msg, test, search, invites, profile, signup, profile_user, post, ScoreView, score, game, index
 
 
 urlpatterns = [
+    path('',index, name='index'),
     path('login/',login, name='login'),
     path('logout/',logout, name='logout'),
     path('play/game/',game, name='game'),
