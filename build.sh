dfa6a5ce8f9ca150a634f91cae83e4ac3df541c9
MrPowerUp82 <gustavogostozo8@gmail.com>
2022-06-12 14:25:51 -0300

    update

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 # build.sh
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', '12345678')" | python manage.py shell
