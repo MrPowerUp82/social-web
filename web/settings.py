e57b9daddd85bc7645a9962b67ecf28e132fbfb7
MrPowerUp82 <gustavohenrique8282@hotmail.com>
2022-06-12 13:28:11 -0300

    update

--- web/settings.py
+++ web/settings.py
@@ -83,14 +83,14 @@ WSGI_APPLICATION = 'web.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'chat_online',
-        'USER': 'root',
-        'PASSWORD': 'toor',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'chat_online',
+    #     'USER': 'root',
+    #     'PASSWORD': 'toor',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '3306',
+    # }
 }
 
 
